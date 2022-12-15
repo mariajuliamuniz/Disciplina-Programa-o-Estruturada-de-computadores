e44728f9e559fa60f707a9ace0c732541f032cdd
Maria Julia <91386297+mariajuliamuniz@users.noreply.github.com>
2022-12-15 09:35:27 -0300

    Add files via upload

--- passo1.py
+++ passo1.py
@@ -1,2 +1,26 @@
-print(12*12.50)
+#lista de letras para criptografar 
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
 
+#a chave secreta é 3
+chave = 3
+
+letra = input("Por favor, entre com uma letra para criptografar: ").lower()
+
+#encontre a posição da letra em alfabeto 
+#exemplo: 'a' está na posição 0, 'e' está na posição 4, etc.
+posicao = alfabeto.find(letra)
+
+#some a chave secretapara encontrar a posição da letra criptografada
+#%26 significa 'volte para 0 quando chegar no 26'
+novaPosicao = (posicao+chave)%26
+
+#a letra criptografada está no alfabeto na nova posição
+letraCriptografada = alfabeto[novaPosicao]
+
+print("Sua letra criptografada é", letraCriptografada)
+
+#descriptografar
+posicao = alfabeto.find(letraCriptografada)
+novaPosicao = (posicao - chave)%26
+letraDescriptografada = alfabeto[novaPosicao]
+print("Sua letra descriptografada é", letraDescriptografada)
